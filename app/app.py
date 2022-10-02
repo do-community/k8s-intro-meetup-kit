95199effff4fda35f2b50c6432f87f7495e2a55f
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2022-10-02 21:25:50 +0300

    modify app route added link

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import urllib.parse
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/sample/')
 def hello_world():
-    return 'Hello, World!'
+   s = urllib.parse.quote('https://github.com/BoazHalter/k8s-hello-world')
+   return 'https://'+s'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
